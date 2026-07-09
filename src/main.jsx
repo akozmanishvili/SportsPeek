0bae8ad3425f0d2289e7cc4ab0ba9e5226f81ae9
AMIKO <akozmanishvili@gmail.com>
2026-07-09 12:25:14 +0400

    feat: added router main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Error from "./pages/Error.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/League",
+        lazy: () =>
+          import("./pages/League.jsx").then((module) => ({
+            element: <module.default></module.default>,
+          })),
+      },
+      {
+        path: "/Player",
+        lazy: () =>
+          import("./pages/Player.jsx").then((module) => ({
+            element: <module.default></module.default>,
+          })),
+      },
+      {
+        path: "/Team",
+        lazy: () =>
+          import("./pages/Team.jsx").then((module) => ({
+            element: <module.default></module.default>,
+          })),
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </StrictMode>,
-)
+);
